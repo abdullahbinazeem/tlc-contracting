577498c1d119b20389ad3dc2c15a5d952d7d59cd
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2024-06-06 23:39:18 -0700

    fixed mobile stylings

--- src/app/pages/services.tsx
+++ src/app/pages/services.tsx
@@ -51,7 +51,7 @@ const serviceList = [
 const Services = () => {
   return (
     <div className="bg-[#EEEEEE]">
-      <div className="max-w-[1800px] m-auto px-10 py-20">
+      <div className="max-w-[1800px] m-auto px-5 sm:px-10 py-20">
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
           {serviceList.map((service) => (
             <a className="" href="#" key={service.title}>
@@ -63,20 +63,20 @@ const Services = () => {
                   className="object-cover group-hover:scale-125 transition-all duration-[300ms]"
                 />
                 <div className="absolute w-full h-full z-10 bg-[#111] opacity-70 group-hover:scale-0 group-hover:rotate-180 transition-all duration-[300ms]" />
-                <div className="absolute group-hover:bottom-10 bottom-[-25px] transition-all left-10 z-20 duration-300">
-                  <div className="flex gap-4 mb-4">
+                <div className="absolute group-hover:bottom-10 bottom-[-25px] transition-all  left-6 sm:left-10 z-20 duration-300">
+                  <div className="flex gap-4 mb-4 items-center">
                     <Image
                       src={`/assets/services/icon/${service.icon}.svg`}
                       alt={service.title + " Icon"}
                       width={42}
                       height={42}
-                      className="text-white"
+                      className="text-white h-8 sm:w-12 h-8 sm:h-12"
                     />
-                    <h1 className=" text-white text-3xl lg:text-4xl font-bold">
+                    <h1 className=" text-white text-2xl sm:text-3xl lg:text-4xl font-bold">
                       {service.title}
                     </h1>
                   </div>
-                  <p className="bg-black inline text-white font-semibold opacity-0 group-hover:opacity-100 transition-all">
+                  <p className="bg-black sm:text-base text-sm inline text-white font-semibold opacity-0 group-hover:opacity-100 transition-all">
                     {service.description}
                   </p>
                 </div>
